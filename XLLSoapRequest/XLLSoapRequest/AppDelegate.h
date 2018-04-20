a1feba70b07878b88551ff775fa899b103eec522
b593771943 <1593771943@qq.com>
2018-04-20 11:08:31 +0800

    Add project

---
+++ XLLSoapRequest/XLLSoapRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLLSoapRequest
+//
+//  Created by 肖乐 on 2018/4/20.
+//  Copyright © 2018年 iOSCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XLLSoapRequest/XLLSoapRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLLSoapRequest
+//
+//  Created by 肖乐 on 2018/4/20.
+//  Copyright © 2018年 iOSCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
